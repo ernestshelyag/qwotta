9b7f980ba70d95102412059a03aeba672aca3485
Eric <ernest.shelyag@gmail.com>
2018-05-17 01:23:48 +0300

    commit

--- assets/gulp/paths/css.vendors.js
+++ assets/gulp/paths/css.vendors.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = [
-  './node_modules/normalize.css/normalize.css'
+  './node_modules/slick-carousel/slick/slick.css'
 ];
--- src/js/main.js
+++ src/js/main.js
@@ -10,11 +10,28 @@ window.onload = () => {
       $('#burger-btn').click();
     }
   });
+  if ($(window).scrollTop() > 70) {
+    $('.header').hide();
+  }
   $('#burger-btn').click(function () {
     $('.burger__lines-block, .burger__mid-line, .sidebar, .sidebar__link').toggleClass('active');
   });
   $('.sidebar').click(function () {
     $('#burger-btn').click();
   });
-  // -
+  // hero slider
+  $('.hero-slider').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev slick-arrow" aria-label="Previous"><span class="decor-prev">ass</span></button>',
+    nextArrow: '<button type="button" class="slick-next slick-arrow" aria-label="Next"><span class="decor-next">piss</span></button>',
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4
+      }
+    }]
+  });
 };
